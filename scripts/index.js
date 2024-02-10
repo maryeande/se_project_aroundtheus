e7dfbd9244b5a1ea7ab1584caf015840d1c51cd6
Mary Anderson <ande9346@gmail.com>
2024-02-10 09:56:16 -0600

    Project 4 Stage 3

--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,15 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileModalClose = document.querySelector("#profile-modal-close");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+profileModalClose.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
